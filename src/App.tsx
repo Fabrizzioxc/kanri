b28dbec504563d922493e59767633b45aef9c18f
Fabrizzioxc <fabrizziozv.17@gmail.com>
2025-08-01 16:15:04 -0500

    fix: cambios de ui para usar componentes de shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Page from "@/app/dashboard/page"
 
-function App() {
+export default function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Page />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -33,8 +33,6 @@ import {
   IconTrendingUp,
 } from "@tabler/icons-react"
 import {
-  ColumnDef,
-  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
   getFacetedRowModel,
@@ -42,11 +40,17 @@ import {
   getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+
+import type {
+  ColumnDef,
+  ColumnFiltersState,
   Row,
   SortingState,
-  useReactTable,
   VisibilityState,
 } from "@tanstack/react-table"
+
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
 import { toast } from "sonner"
 import { z } from "zod"
@@ -55,11 +59,12 @@ import { useIsMobile } from "@/hooks/use-mobile"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import {
-  ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart"
+import type { ChartConfig } from "@/components/ui/chart"
+
 import { Checkbox } from "@/components/ui/checkbox"
 import {
   Drawer,
